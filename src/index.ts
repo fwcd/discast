67d4639fcfeb8bc156c32592d9b4c3b5ad49daf0
fwcd <fwcdmail@gmail.com>
2022-03-25 23:27:09 +0100

    Connect to voice channel

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello");
+import * as process from 'process';
+import { Client, Intents, VoiceChannel } from 'discord.js';
+import { createAudioPlayer, joinVoiceChannel } from '@discordjs/voice';
+
+function getEnv(name: string): string {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Please make sure that ${name} is set!`);
+  }
+  return value;
+}
+
+// Read config from env
+const botToken = getEnv('BOT_TOKEN');
+const voiceChannelId = getEnv('VOICE_CHANNEL_ID');
+const streamUrl = getEnv('STREAM_URL');
+
+const client = new Client({ intents: [] });
+
+client.on('ready', async () => {
+  const channel = await client.channels.fetch(voiceChannelId) as VoiceChannel;
+  if (!channel.isVoice()) {
+    throw new Error('Not a voice channel!');
+  }
+  const connection = joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guildId,
+    adapterCreator: channel.guild.voiceAdapterCreator
+  });
+});
+
+// Connect to Discord
+client.login(botToken);
